cd1101b85ed2ffc111aaeff7f4e2cdb05a9f491d
Eric Saxby <sax@livinginthepast.org>
2022-05-21 15:48:40 -0700

    Add revision to dyd formula

--- Formula/dyd.rb
+++ Formula/dyd.rb
@@ -4,6 +4,7 @@ class Dyd < Formula
   license "MIT"
   sha256 "b5a323a98cc61c35cd7d751bf81b67239b55e72f1919cd541a3ec51a39f481c5"
   url "https://github.com/synchronal/dyd/releases/download/v0.3.1/dyd-darwin-arm64.tar.gz"
+  revision 2
 
   def install
     bin.install "dyd"
