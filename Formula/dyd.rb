7db394884baf75d238c8a038e9247c576d2d49ef
Eric Saxby <sax@livinginthepast.org>
2022-07-23 11:47:23 -0700

    Install dyd universal binary

--- Formula/dyd.rb
+++ Formula/dyd.rb
@@ -2,9 +2,9 @@ class Dyd < Formula
   desc "Daily Diff"
   homepage "https://github.com/synchronal/dyd"
   license "MIT"
-  sha256 "3a45128366360e9732a2249e0fbcbf5c4bc0cd1140401193af14af8f7a183735"
-  url "https://github.com/synchronal/dyd/releases/download/v0.3.1/dyd-darwin-arm64.tar.gz"
-  revision 2
+  sha256 "f3de40760ed6a2023526a4608a20aee696bca04d0efa02392146cdfc640b837a"
+  url "https://github.com/synchronal/dyd/releases/download/v0.3.1/dyd-darwin-universal.tar.gz"
+  revision 3
 
   def install
     bin.install "dyd"
