18b96c2cc1f8ad83f964afec7cf405ac80860d0e
Eric Saxby <sax@livinginthepast.org>
2022-08-11 09:21:09 -0700

    Update dyd to v0.4.0

--- Formula/dyd.rb
+++ Formula/dyd.rb
@@ -2,8 +2,8 @@ class Dyd < Formula
   desc "Daily Diff"
   homepage "https://github.com/synchronal/dyd"
   license "MIT"
-  sha256 "f3de40760ed6a2023526a4608a20aee696bca04d0efa02392146cdfc640b837a"
-  url "https://github.com/synchronal/dyd/releases/download/v0.3.1/dyd-darwin-universal.tar.gz"
+  sha256 "943fc492c117acb10ff3964b6da710c87fe3f41ac18ee5d53f770874ff0c2134"
+  url "https://github.com/synchronal/dyd/releases/download/v0.4.0/dyd-darwin-universal.tar.gz"
   revision 3
 
   def install
