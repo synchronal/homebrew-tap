5df7b395593e0611971d9573222b6be565c95b3f
Eric Saxby <sax@livinginthepast.org>
2022-11-29 11:46:23 -0800

    Update dyd to 1.1.1

--- Formula/dyd.rb
+++ Formula/dyd.rb
@@ -2,10 +2,10 @@ class Dyd < Formula
   desc "Daily Diff"
   homepage "https://github.com/synchronal/dyd"
   license "MIT"
-  sha256 "8164a3b217e138cde67dfc2c4b092de7d915d38a1f10e6a8ad0c418d57e7885f"
-  url "https://github.com/synchronal/dyd/releases/download/v1.1.0/dyd-darwin-universal.tar.gz"
+  sha256 "f83c664dca1240052fc975fbbd1be57954e01094606f65ee2d68fa19ac2958a3"
+  url "https://github.com/synchronal/dyd/releases/download/v1.1.1/dyd-darwin-universal.tar.gz"
   revision 1
-  version "1.1.0"
+  version "1.1.1"
 
   def install
     bin.install "dyd"
