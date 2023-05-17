ac924c4c213359e3480c5843ab0880f8fb7a8619
Eric Saxby <sax@livinginthepast.org>
2023-05-17 10:28:29 -0700

    medic-elixir uses its own repo

--- Formula/medic-elixir.rb
+++ Formula/medic-elixir.rb
@@ -1,11 +1,11 @@
 class MedicElixir < Formula
   desc "Medic language pack: Elixir"
-  homepage "https://github.com/synchronal/medic-rs"
+  homepage "https://github.com/synchronal/medic-ext-elixir"
   license "MIT"
-  sha256 "f0d75f6eea1701b495efc071e2ea9a2d1e796a85d04f3731d62c7b95dc55de1b"
-  url "https://github.com/synchronal/medic-rs/releases/download/v0.2.0/medic-elixir-darwin-universal.tar.gz"
+  sha256 "0d7f0625c734bd526e8383b10474505316377e999a3c5c6e1d7fd58bb5f53d6e"
+  url "https://github.com/synchronal/medic-ext-elixir/releases/download/v0.1.0/medic-elixir-darwin-universal.tar.gz"
   revision 0
-  version "0.2.0"
+  version "0.1.0"
 
   def install
     bin.install "medic-check-elixir"
