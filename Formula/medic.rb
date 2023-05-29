a7b1ef727b06af0bd9af2958ecd7d30da01fc8f3
Eric Saxby <sax@livinginthepast.org>
2023-05-29 10:42:02 -0700

    Update medic to 0.3.1

--- Formula/medic.rb
+++ Formula/medic.rb
@@ -2,10 +2,10 @@ class Medic < Formula
   desc "Medic development workflow management"
   homepage "https://github.com/synchronal/medic-rs"
   license "MIT"
-  sha256 "604314280013ea477731b6fc76e5eb309bfa9df49e8db494d2bd0167879af7e1"
-  url "https://github.com/synchronal/medic-rs/releases/download/v0.2.1/medic-darwin-universal.tar.gz"
+  sha256 "81a8a05a5acae5ea244b9f34f0df83025aa4b750687ff0030b8f0efafd24d3e1"
+  url "https://github.com/synchronal/medic-rs/releases/download/v0.3.1/medic-darwin-universal.tar.gz"
   revision 0
-  version "0.2.1"
+  version "0.3.1"
 
   depends_on "git"
 
