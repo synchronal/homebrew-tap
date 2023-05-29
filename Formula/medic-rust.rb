738c3f01beff73ff3b1fe7bf9de0ab781384fa6b
Eric Saxby <sax@livinginthepast.org>
2023-05-29 11:22:36 -0700

    Update medic-rust to 0.2.1; use separate repo

--- Formula/medic-rust.rb
+++ Formula/medic-rust.rb
@@ -2,10 +2,10 @@ class MedicRust < Formula
   desc "Medic language pack: Rust"
   homepage "https://github.com/synchronal/medic-rs"
   license "MIT"
-  sha256 "4c37751e5646b4ef250c51042d6016e5705202e52d1f46d14ac1747429d0aa4b"
-  url "https://github.com/synchronal/medic-rs/releases/download/v0.2.0/medic-rust-darwin-universal.tar.gz"
+  sha256 "3563fada928bba3b51c93e35d599b44417dd8824f3da562759c934038ad275cc"
+  url "https://github.com/synchronal/medic-ext-rust/releases/download/v0.2.1/medic-ext-rust-darwin-universal.tar.gz"
   revision 0
-  version "0.2.0"
+  version "0.2.1"
 
   def install
     bin.install "medic-check-rust"
