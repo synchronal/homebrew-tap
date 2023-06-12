284eae6df80296eacad6cb0a0ddaacb73969dd3f
Eric Saxby <sax@livinginthepast.org>
2023-06-12 09:08:52 -0700

    Update dyd to 1.5.3

--- Formula/dyd.rb
+++ Formula/dyd.rb
@@ -2,10 +2,10 @@ class Dyd < Formula
   desc "Daily Diff"
   homepage "https://github.com/synchronal/dyd"
   license "MIT"
-  sha256 "2f325a2994ebea4f1ed4cc64888465e6c2dc994a86815185d6371b6829e2310a"
-  url "https://github.com/synchronal/dyd/releases/download/v1.5.2/dyd-darwin-universal.tar.gz"
+  sha256 "5f61e6babab77e1d394cdc8bf45e543d3641cd7bb83f17e03425110982df8c65"
+  url "https://github.com/synchronal/dyd/releases/download/v1.5.3/dyd-darwin-universal.tar.gz"
   revision 0
-  version "1.5.2"
+  version "1.5.3"
 
   depends_on "git"
 
