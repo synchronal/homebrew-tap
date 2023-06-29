3b8e9b39dcfc44c3666ed07800c712a2df1e196a
Eric Saxby <sax@livinginthepast.org>
2023-06-29 09:00:51 -0700

    Rename medic-elixir -> medic-ext-elixir, medic-rust -> medic-ext-rust

--- Formula/medic-ext-elixir.rb
+++ Formula/medic-ext-elixir.rb
@@ -1,13 +1,14 @@
-class MedicElixir < Formula
-  deprecate! date: "2023-06-29", because: "has been replaced with synchronal/tap/medic-ext-elixir"
-
+class MedicExtElixir < Formula
   desc "Medic language pack: Elixir"
   homepage "https://github.com/synchronal/medic-ext-elixir"
   license "MIT"
   sha256 "0d7f0625c734bd526e8383b10474505316377e999a3c5c6e1d7fd58bb5f53d6e"
   url "https://github.com/synchronal/medic-ext-elixir/releases/download/v0.1.0/medic-elixir-darwin-universal.tar.gz"
-  revision 1
+  revision 0
   version "0.1.0"
 
-  depends_on "medic-ext-elixir"
+  def install
+    bin.install "medic-check-elixir"
+    bin.install "medic-step-elixir"
+  end
 end
--- Formula/medic-ext-rust.rb
+++ Formula/medic-ext-rust.rb
@@ -1,13 +1,14 @@
-class MedicRust < Formula
-  deprecate! date: "2023-06-29", because: "has been replaced with synchronal/tap/medic-ext-rust"
-
+class MedicExtRust < Formula
   desc "Medic language pack: Rust"
   homepage "https://github.com/synchronal/medic-rs"
   license "MIT"
   sha256 "3563fada928bba3b51c93e35d599b44417dd8824f3da562759c934038ad275cc"
   url "https://github.com/synchronal/medic-ext-rust/releases/download/v0.2.1/medic-ext-rust-darwin-universal.tar.gz"
-  revision 1
+  revision 0
   version "0.2.1"
 
-  depends_on "medic-ext-rust"
+  def install
+    bin.install "medic-check-rust"
+    bin.install "medic-step-rust"
+  end
 end
