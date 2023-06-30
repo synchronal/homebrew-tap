081c356f259565a1e8173786fdf05e1751128cf5
Eric Saxby <sax@livinginthepast.org>
2023-06-30 09:16:26 -0700

    Update medic-ext-node to 0.2.0

--- Formula/medic-ext-node.rb
+++ Formula/medic-ext-node.rb
@@ -2,10 +2,10 @@ class MedicExtNode < Formula
   desc "Medic language pack: NodeJS"
   homepage "https://github.com/synchronal/medic-ext-node"
   license "MIT"
-  sha256 "dcc03cc17adefa356a64ddc258ff27d9cdb92ebddc8a0d411085a8e9ac994f92"
-  url "https://github.com/synchronal/medic-ext-node/releases/download/v0.1.0/medic-ext-node-darwin-universal.tar.gz"
+  sha256 "2081b17235a99260256eefb97dd709f02e9b13ddf6ca6e4bbae405a42b27e45a"
+  url "https://github.com/synchronal/medic-ext-node/releases/download/v0.2.0/medic-ext-node-darwin-universal.tar.gz"
   revision 0
-  version "0.1.0"
+  version "0.2.0"
 
   def install
     bin.install "medic-check-node"
