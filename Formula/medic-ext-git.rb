dc31eb96a3fef3e40357b0575e7ed838adc334f3
Eric Saxby <sax@livinginthepast.org>
2023-07-20 06:41:22 +0800

    Add medic-ext-git: v0.2.1

---
+++ Formula/medic-ext-git.rb
@@ -0,0 +1,16 @@
+class MedicExtGit < Formula
+  desc "Medic language pack: Git"
+  homepage "https://github.com/synchronal/medic-ext-git"
+  license "MIT"
+  sha256 "c878b0a63b700cff07481ca21ea3f0d09fb8f0e97bb2ac9571810801c1d5e710"
+  url "https://github.com/synchronal/medic-ext-git/releases/download/v0.2.1/medic-ext-git-darwin-universal.tar.gz"
+  revision 0
+  version "0.2.1"
+
+  depends_on "medic"
+
+  def install
+    bin.install "medic-step-git"
+    bin.install "medic-step-github"
+  end
+end
