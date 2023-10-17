5323d7ba1ac17259c05733b4b09c8665efbda99d
Eric Saxby <sax@livinginthepast.org>
2023-10-17 16:31:48 -0700

    Update medic-rs to v1.0.0

--- Formula/medic-rs.rb
+++ Formula/medic-rs.rb
@@ -2,10 +2,10 @@ class MedicRs < Formula
   desc "Medic development workflow management"
   homepage "https://github.com/synchronal/medic-rs"
   license "MIT"
-  sha256 "a88a0e2152b6c33490a5b0d4110fd9d4c686b1462e3a657e81b6b58a4618acd0"
-  url "https://github.com/synchronal/medic-rs/releases/download/v0.5.0/medic-darwin-universal.tar.gz"
+  sha256 "b5d1c7e3244f0d762c1d9685a1062f74162b6805fcc9e39ccb35a221a8772772"
+  url "https://github.com/synchronal/medic-rs/releases/download/v1.0.0/medic-darwin-universal.tar.gz"
   revision 0
-  version "0.5.0"
+  version "1.0.0"
 
   depends_on "git"
 
@@ -13,6 +13,7 @@ class MedicRs < Formula
     bin.install "medic"
     bin.install "medic-audit"
     bin.install "medic-doctor"
+    bin.install "medic-init"
     bin.install "medic-shipit"
     bin.install "medic-test"
     bin.install "medic-update"
