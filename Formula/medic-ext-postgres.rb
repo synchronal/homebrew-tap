a8bf5aaeb4bcf987ef10ab878f5515ba6e2abb76
Eric Saxby <sax@livinginthepast.org>
2023-11-11 11:42:55 -0800

    Update medic-ext-postgres to v1.0.0

--- Formula/medic-ext-postgres.rb
+++ Formula/medic-ext-postgres.rb
@@ -2,10 +2,12 @@ class MedicExtPostgres < Formula
   desc "Medic extension pack: Postgres"
   homepage "https://github.com/synchronal/medic-ext-postgres"
   license "MIT"
-  sha256 "cf30f1fa413e98ba222900bf426edea806ef20c9c301e8cc816d348ab7490525"
-  url "https://github.com/synchronal/medic-ext-postgres/releases/download/v0.1.1/medic-ext-postgres-darwin-universal.tar.gz"
-  revision 1
-  version "0.1.1"
+
+  revision 0
+  sha256 "a8409d555d28f5cf6e57e7bddd2f58fe075ba014f33ded3599247ef65d5a46e3"
+  version "1.0.0"
+
+  url "https://github.com/synchronal/medic-ext-postgres/releases/download/v#{version}/medic-ext-postgres-darwin-universal.tar.gz"
 
   depends_on "medic-rs"
   depends_on "medic-bash"
