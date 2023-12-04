75f14a1393f1011f07614ca67ad0912b38279c51
Eric Saxby <sax@livinginthepast.org>
2023-12-04 10:50:15 -0800

    Upgrade medic-ext-elixir to v0.2.0

--- Formula/medic-ext-elixir.rb
+++ Formula/medic-ext-elixir.rb
@@ -2,15 +2,18 @@ class MedicExtElixir < Formula
   desc "Medic language pack: Elixir"
   homepage "https://github.com/synchronal/medic-ext-elixir"
   license "MIT"
-  sha256 "0d7f0625c734bd526e8383b10474505316377e999a3c5c6e1d7fd58bb5f53d6e"
-  url "https://github.com/synchronal/medic-ext-elixir/releases/download/v0.1.0/medic-elixir-darwin-universal.tar.gz"
-  revision 1
-  version "0.1.0"
+
+  revision 0
+  sha256 "44a6c55d29c7b578c998ae6cd4b40f9ca40621639d4f7f456f443d6284f95c9a"
+  version "0.2.0"
+
+  url "https://github.com/synchronal/medic-ext-elixir/releases/download/v#{version}/medic-elixir-darwin-universal.tar.gz"
 
   depends_on "medic-rs"
 
   def install
     bin.install "medic-check-elixir"
+    bin.install "medic-outdated-elixir"
     bin.install "medic-step-elixir"
   end
 end
