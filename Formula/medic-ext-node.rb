38f50f70cf87f1e80953cd8a2902d572248fe4ed
Eric Saxby <sax@livinginthepast.org>
2024-05-14 10:55:29 -0700

    Update medic-ext-node to v0.4.0

--- Formula/medic-ext-node.rb
+++ Formula/medic-ext-node.rb
@@ -4,8 +4,8 @@ class MedicExtNode < Formula
   license "MIT"
 
   revision 0
-  sha256 "8e1af6e9e264fd622b535e894744d7c7fddad19523bacb2f8eba19df584baf4c"
-  version "0.3.0"
+  sha256 "76c782c91736e03d8980840f273dd7231da41350618afb3e8832b322e874ddbc"
+  version "0.4.0"
 
   url "https://github.com/synchronal/medic-ext-node/releases/download/v#{version}/medic-ext-node-darwin-universal.tar.gz"
 
@@ -13,5 +13,6 @@ class MedicExtNode < Formula
 
   def install
     bin.install "medic-check-node"
+    bin.install "medic-outdated-node"
   end
 end
