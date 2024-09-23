e34d75d5b6c5dfb0777324c9e46193426cc92201
Eric Saxby <sax@livinginthepast.org>
2024-09-23 10:46:42 -0700

    Update medic to v2.8.0

--- Formula/medic-rs.rb
+++ Formula/medic-rs.rb
@@ -4,8 +4,8 @@ class MedicRs < Formula
   license "MIT"
 
   revision 0
-  sha256 "0a1b06acc135629c4a8aa7ab5d2de5abd34da56654b105fc4fa695ba5c76150e"
-  version "2.7.0"
+  sha256 "efbb10034c1d300e5d1731787efffbecaa2b81deab2bd00fa8c99d1020d1e965"
+  version "2.8.0"
 
   url "https://github.com/synchronal/medic-rs/releases/download/v#{version}/medic-darwin-universal.tar.gz"
 
