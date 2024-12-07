6253bc8105a85d36c26fa848f5af064d21edb955
Eric Saxby <sax@livinginthepast.org>
2024-12-06 17:42:24 -0800

    Update dyd to v1.8.6

--- Formula/dyd.rb
+++ Formula/dyd.rb
@@ -4,8 +4,8 @@ class Dyd < Formula
   license "MIT"
 
   revision 0
-  sha256 "20d0b05e62be2d9d47688310bceb2d7581641fbd5a9e561c3c73ab7370ffec66"
-  version "1.8.4"
+  sha256 "dd198741491bae13c0c57339e9beb533725c2d1786919085250d7233bd89d5d9"
+  version "1.8.6"
 
   url "https://github.com/synchronal/dyd/releases/download/v#{version}/dyd-darwin-universal.tar.gz"
 
