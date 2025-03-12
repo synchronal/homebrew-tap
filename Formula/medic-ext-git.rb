761ac6a997134ad7c565ef4df054a3485da5ad59
Eric Saxby <sax@livinginthepast.org>
2025-03-11 22:32:40 -0700

    Update medic-ext-git to v0.3.0

--- Formula/medic-ext-git.rb
+++ Formula/medic-ext-git.rb
@@ -2,10 +2,12 @@ class MedicExtGit < Formula
   desc "Medic language pack: Git"
   homepage "https://github.com/synchronal/medic-ext-git"
   license "MIT"
-  sha256 "7c3333923cc26ca1308757c059927c2373537be63aaa694254afedcf34af6d81"
-  url "https://github.com/synchronal/medic-ext-git/releases/download/v0.2.2/medic-ext-git-darwin-universal.tar.gz"
+
   revision 0
-  version "0.2.2"
+  sha256 "06ef11f2b4b4a110a666a229e6c2ba39ca259d0b553e586386d6b926d5380b49"
+  version "0.3.0"
+
+  url "https://github.com/synchronal/medic-ext-git/releases/download/v#{version}/medic-ext-git-darwin-universal.tar.gz"
 
   depends_on "medic-rs"
 
