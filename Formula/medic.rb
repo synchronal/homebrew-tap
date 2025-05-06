5ebfcfd5cc243e55083d43d762ac56b375027da0
Eric Saxby <sax@livinginthepast.org>
2025-05-06 14:28:09 -0700

    Remove sha from medic shell formula

--- Formula/medic.rb
+++ Formula/medic.rb
@@ -5,7 +5,6 @@ class Medic < Formula
   url "https://github.com/synchronal/medic-rs"
 
   revision 0
-  sha256 "2f92dde93ef89239d51d7a09af32838f12a66d9921e711b0602cbe64da159fdd"
   version "2.10.0"
 
   depends_on "medic-rs"
