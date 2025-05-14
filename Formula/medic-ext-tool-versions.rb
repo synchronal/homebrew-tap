a3feafa031b78ade5b1cc328a1d2c70ec466172b
Eric Saxby <sax@livinginthepast.org>
2025-05-14 11:26:39 -0700

    Update medic-ext-tool-versions to v1.0.0

--- Formula/medic-ext-tool-versions.rb
+++ Formula/medic-ext-tool-versions.rb
@@ -4,8 +4,8 @@ class MedicExtToolVersions < Formula
   license "MIT"
 
   revision 0
-  sha256 "703d19adb52ddaec319bbcc4fd8dc73bd92c6fbdf8a4916622a63174fafb616d"
-  version "0.5.0"
+  sha256 "6c3bd686aab1d173219b4980a0d35649f298ab158d770a7af76ce66c0423fa4b"
+  version "1.0.0"
 
   url "https://github.com/synchronal/medic-ext-tool-versions/releases/download/v#{version}/medic-ext-tool-versions-darwin-universal.tar.gz"
 
