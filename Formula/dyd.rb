7561dfeaf01df7f84cb055583a9affa55a82c9e6
Eric Saxby <sax@livinginthepast.org>
2026-01-23 08:31:02 -0800

    Update dyd to 1.11.0; install platform-specific version

--- Formula/dyd.rb
+++ Formula/dyd.rb
@@ -2,12 +2,17 @@ class Dyd < Formula
   desc "Daily Diff"
   homepage "https://github.com/synchronal/dyd"
   license "MIT"
+  version "1.11.0"
 
-  revision 0
-  sha256 "737f562cce1c3c999a926d01a21ca9a8e62e975af82801c6abc7e55df82a8538"
-  version "1.10.0"
+  on_arm do
+    url "https://github.com/synchronal/dyd/releases/download/v#{version}/dyd-darwin-arm64.tar.gz"
+    sha256 "73614134cd91e76099cffeef97f16a3cc3da14c0308cb141cee0ceca2316d588"
+  end
 
-  url "https://github.com/synchronal/dyd/releases/download/v#{version}/dyd-darwin-universal.tar.gz"
+  on_intel do
+    url "https://github.com/synchronal/dyd/releases/download/v#{version}/dyd-darwin-x86_64.tar.gz"
+    sha256 "89547ab10e59a91f7aa59d337bacf7875f3f629ad359329bfa457b1bd0f201b6"
+  end
 
   depends_on "git"
 
