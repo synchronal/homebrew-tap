b33edc265a497c146129cad6587304cc32902c3e
Eric Saxby <sax@livinginthepast.org>
2026-01-29 13:24:09 -0800

    Update dyd to v1.11.1

--- Formula/dyd.rb
+++ Formula/dyd.rb
@@ -2,16 +2,16 @@ class Dyd < Formula
   desc "Daily Diff"
   homepage "https://github.com/synchronal/dyd"
   license "MIT"
-  version "1.11.0"
+  version "1.11.1"
 
   on_arm do
     url "https://github.com/synchronal/dyd/releases/download/v#{version}/dyd-darwin-arm64.tar.gz"
-    sha256 "73614134cd91e76099cffeef97f16a3cc3da14c0308cb141cee0ceca2316d588"
+    sha256 "76b2342f2e60b9550490938a28e1d8541f400cce664f44eab1b812b9f078da78"
   end
 
   on_intel do
     url "https://github.com/synchronal/dyd/releases/download/v#{version}/dyd-darwin-x86_64.tar.gz"
-    sha256 "89547ab10e59a91f7aa59d337bacf7875f3f629ad359329bfa457b1bd0f201b6"
+    sha256 "6150fd289a687153f9ec52743497d9e9f03464f21ad133fe6cde4e60551d83c5"
   end
 
   depends_on "git"
