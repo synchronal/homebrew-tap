72ca8b93d5f4488b4ee92cc1e16186b84beb096f
Eric Saxby <sax@livinginthepast.org>
2026-02-11 13:38:45 -0800

    Update medic to v2.21.1

--- Formula/medic-rs.rb
+++ Formula/medic-rs.rb
@@ -4,16 +4,16 @@ class MedicRs < Formula
   license "MIT"
 
   revision 0
-  version "2.21.0"
+  version "2.21.1"
 
   on_arm do
     url "https://github.com/synchronal/medic-rs/releases/download/v#{version}/medic-darwin-arm64.tar.gz"
-    sha256 "5242a05f5073d0f24218d2b001a5c84d135b71de8f4415a3233428c70e5c95dc"
+    sha256 "2d3b5c1250648b74b102107ade9ba8fdf4551d7d678ea35caf4acc87ad67cf48"
   end
 
   on_intel do
     url "https://github.com/synchronal/medic-rs/releases/download/v#{version}/medic-darwin-x86_64.tar.gz"
-    sha256 "e6abda684b623548dcaa78bb6610ee0fdb7747d26c34671d875f5c875fa85f89"
+    sha256 "f66d2722fa7f80444342de283ca3b54bd9d27da17f36e5d7595430bbdfe22e1e"
   end
 
   depends_on "git"
