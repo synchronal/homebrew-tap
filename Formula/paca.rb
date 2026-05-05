469e56a7874d16619af10151ba303a5bd1943353
Eric Saxby <sax@livinginthepast.org>
2026-05-04 18:26:42 -0700

    Update paca to v0.1.2

--- Formula/paca.rb
+++ Formula/paca.rb
@@ -4,16 +4,16 @@ class Paca < Formula
   license "MIT"
 
   revision 0
-  version "0.1.1"
+  version "0.1.2"
 
   on_arm do
     url "https://github.com/synchronal/paca/releases/download/v#{version}/paca-darwin-arm64.tar.gz"
-    sha256 "83835090a082b5e14c45bcc0b4fa7302598054686ace1e727672022f07684277"
+    sha256 "3b56a72571a67b1db51f0b15deef13ece2def28181c3c65a7adfa41d0f33a270"
   end
 
   on_intel do
     url "https://github.com/synchronal/paca/releases/download/v#{version}/paca-darwin-x86_64.tar.gz"
-    sha256 "a21c2852871b17d93f34b6f499f4ba41c59d04bc84274dd5121e4cada7d5006e"
+    sha256 "4fc6f3116cd42eb7be94c4e96ac87bd1411ed4618ee165ab5b933c3d292e1186"
   end
 
   def install
